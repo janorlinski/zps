58a96669979d7b0139f4be22e4a1101175cd645b
janorlinski <91850084+janorlinski@users.noreply.github.com>
2023-05-23 10:57:25 +0200

    Add files via upload

--- convertTextToHisto.C
+++ convertTextToHisto.C
@@ -8,6 +8,74 @@
 #include "TFile.h"
 #include "TSpectrum.h"
 #include "TSpectrumFit.h"
+#include "TF1.h"
+#include "TStopwatch.h"
+
+Double_t gausswithlinearbkg (Double_t *xarg, Double_t *par)
+{
+ Double_t x = xarg[0] , result = 0.;
+ result = par[0]/(par[2]*TMath::Sqrt(2*TMath::Pi())) * TMath::Exp((-TMath::Power((x-par[1]),2))/(2*TMath::Power(par[2],2))) + par[3]*x + par[4];
+ return result;
+}
+
+
+void fitTwoGaussPeaksToHisto (TH1I Histogram) {
+	
+	std::cout << "fitting two Gauss peaks to file " << Histogram.GetName() << std::endl;
+	
+	int i, NFound, NBins, binnum;
+	NBins = Histogram.GetNbinsX();
+	int loCut = 2000;
+	int hiCut = 5500;
+	double fitrange = 50.0;
+	//NBinsTrue = NBins - (loCut + hiCut);
+	
+	Double_t* source = new Double_t [NBins];
+	Double_t* destin = new Double_t [NBins];
+	
+	for (i = 0; i < NBins; i++) {
+		source[i] = Histogram.GetBinContent (i+1);
+		if (i<loCut || i>hiCut) source[i] = 0;
+		//cout << i << ": " << source[i] << endl;
+		
+	}
+	
+	TSpectrum* Analyser1dim = new TSpectrum (2); 
+	NFound = Analyser1dim->SearchHighRes (source, destin, NBins, 10.0, 50.0, kFALSE, 100, kFALSE, 0);
+
+	cout << "SearchHighRes has found: " << NFound << " peaks \n";
+
+	if (NFound < 2 || NFound > 2) {
+		std::cout << "N peaks not eq to 2, abort fitting \n";
+		return;
+	}
+
+	Double_t* Posit  = Analyser1dim->GetPositionX ();
+	//Double_t* Amplit = new Double_t [NFound];
+
+	TF1* dopasowanie1 = new TF1("dopasowanie1", gausswithlinearbkg, Posit[0]-fitrange, Posit[0]+fitrange, 5);
+	TF1* dopasowanie2 = new TF1("dopasowanie2", gausswithlinearbkg, Posit[1]-fitrange, Posit[1]+fitrange, 5);
+           
+        dopasowanie1->SetParameters(1000, Posit[0], 10, 0.0000000001, 0.00001);
+        dopasowanie1->SetParNames("Amplituda", "Srednia", "Sigma", "A", "B");
+
+        dopasowanie2->SetParameters(1000, Posit[1], 10, 0.0000000001, 0.00001);
+        dopasowanie2->SetParNames("Amplituda", "Srednia", "Sigma", "A", "B");
+          
+      	Histogram.Fit("dopasowanie1","MIQR","",Posit[0]-fitrange, Posit[0]+fitrange); //MIQR
+      	Histogram.Fit("dopasowanie2","MIQR","",Posit[1]-fitrange,Posit[1]+fitrange); //MIQR
+         
+    	Double_t sigma1 = dopasowanie1->GetParameter(2);
+    	Double_t sigma2 = dopasowanie2->GetParameter(2);
+
+	std::cout << "Found position: " << dopasowanie1->GetParameter(1) << " +/- " << dopasowanie1->GetParError(1) << "\n";
+	std::cout << "Found position: " << dopasowanie2->GetParameter(1) << " +/- " << dopasowanie2->GetParError(1) << "\n";
+	
+	dopasowanie1->Write();
+	dopasowanie2->Write();
+
+}
+
 
 TH1I getHistoFromTxtFile (TString fileName) {
 	
@@ -78,6 +146,7 @@ vector<TH1I> getHistosFromTxtFileList (TString fileList) {
 		if (verbatim) cout << "Loading txt file '" << fileName << "'. \n"; 
 		TH1I histo = getHistoFromTxtFile(fileName);
 		histo.SetName(fileName);
+		fitTwoGaussPeaksToHisto(histo);
 		histo.Write();
 		histos.push_back(histo);
 	}
@@ -87,102 +156,24 @@ vector<TH1I> getHistosFromTxtFileList (TString fileList) {
 	return histos;
 }
 
-void fitTwoGaussPeaksToHisto (TH1I Histogram) {
-	
-	//std::cout << "hello this is fitTwoGauss..." << std::endl;
-	
-	int i, NFound, NBins, binnum;
-	NBins = Histogram.GetNbinsX();
-	int loCut = 200;
-	int hiCut = 30000;
-	//NBinsTrue = NBins - (loCut + hiCut);
-	
-	Double_t* source = new Double_t [NBins];
-	Double_t* destin = new Double_t [NBins];
-	
-	for (i = 0; i < NBins; i++) {
-		source[i] = Histogram.GetBinContent (i+1);
-		if (i<loCut || i>hiCut) source[i] = 0;
-		//cout << i << ": " << source[i] << endl;
-		
-	}
-	
-	TSpectrum* Analyser1dim = new TSpectrum (2); 
-	NFound = Analyser1dim->SearchHighRes (source, destin, NBins, 10.0, 50.0, kFALSE, 100, kFALSE, 0);
-
-	cout << "SearchHighRes has found: " << NFound << " peaks \n";
-
-	Bool_t* FixPosit  = new Bool_t [NFound];          // should these parameters be fixed? [T/F]
-	Bool_t* FixAmplit = new Bool_t [NFound];
-
-	Double_t* Posit  = Analyser1dim->GetPositionX ();
-	Double_t* Amplit = new Double_t [NFound];
-
-	for (i = 0; i < NFound; i++) {
-		
-		FixAmplit[i] = kFALSE;
-		FixPosit [i] = kFALSE;
-		binnum = int(Posit[i]);            // the "nearest" bin
-		Amplit[i] = Histogram.GetBinContent(binnum);   // estimating the peak amplitude
-		
-	}
-
-	TSpectrumFit* Fitter1dim = new TSpectrumFit (NFound);
-	Fitter1dim->SetFitParameters (loCut, hiCut, 1000, 0.1, Fitter1dim->kFitOptimChiCounts,
-                                Fitter1dim->kFitAlphaHalving, Fitter1dim->kFitPower2,
-                                Fitter1dim->kFitTaylorOrderFirst );
-                                
-	Fitter1dim->SetPeakParameters (10., kFALSE, Posit, FixPosit, Amplit, FixAmplit);                                
-	Fitter1dim->FitAwmi (source);
-		
-	Double_t* Positions        = Fitter1dim-> GetPositions ();       // Caution: Xscale is Bin No.
-	//Double_t* PositionsErrors  = Fitter1dim-> GetPositionsErrors (); // Caution: Xscale is Bin No.
-	//Double_t* Amplitudes       = Fitter1dim-> GetAmplitudes ();
-	//Double_t* AmplitudesErrors = Fitter1dim-> GetAmplitudesErrors ();
-	//Double_t* Areas            = Fitter1dim-> GetAreas ();           // Caution: Xscale is Bin No.
-	//Double_t* AreasErrors      = Fitter1dim-> GetAreasErrors ();     // Caution: Xscale is Bin No.
-  
-	//Double_t x1 = Histogram.GetBinCenter(1);
-	//Double_t dx = Histogram.GetBinWidth (1);
-	
-	//cout << "\n * Overall sigma found by fit: " << sigma << " (+-" << sigmaErr << ")" << endl;
-	//cout << " * Fit chi^2 = " << Fitter1dim->GetChi () << "\n\n";
- 
- 
-   for (i = 0; i < NFound; i++)
-  {
-    // Convert "bin numbers" into "x-axis values"
-    //Positions      [i] = x1 + Positions[i] * dx;
-    //PositionsErrors[i] *= dx;
-    //Areas          [i] *= dx;
-    //AreasErrors    [i] *= dx;
-
-    //cout << "Found: "
-         //<< Positions [i] << " (+-" << PositionsErrors [i] << ") "
-         //<< Amplitudes[i] << " (+-" << AmplitudesErrors[i] << ") "
-         //<< Areas     [i] << " (+-" << AreasErrors     [i] << ")" << endl;
-         
-		std::cout << "Found position: " << Positions[i] << std::endl;
-  }
-  
-  
-  
-	
-}
-
 int convertTextToHisto () {
 
-	TString listName = "lista_jedenplik.txt";
+	TStopwatch t;
+	t.Start();
+
+	TString listName = "lista_10umRun20.list";
 	std::cout << "Selected listName '" << listName << "'\n";
 	
 	std::vector<TH1I> histos = getHistosFromTxtFileList(listName);
 	Int_t nHistos = histos.size();
 	//std::cout << histos[0].GetNbinsX() << std::endl;
 
-	for (Int_t i=0; i<nHistos; i++) {
-		fitTwoGaussPeaksToHisto(histos[i]);
-	}
+	//for (Int_t i=0; i<nHistos; i++) {
+		//fitTwoGaussPeaksToHisto(histos[i]);
+	//}
 	
+	Double_t time = t.RealTime();
+	std::cout << "Done analyzing " << nHistos << " spectra in " << time << " seconds. Av = " << time/nHistos << " seconds/spec. \n";
 	return 0;
-	
+
 }
